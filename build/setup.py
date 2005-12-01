8b461ccf721538f57a11ba78c3d31b2fa3f8de57
Paul <Paul@9bf210a0-9d2d-494c-87cf-cfb32e7dff7b>
2005-12-01 05:26:50 +0000

    Added items remotely
    C:\temp\load\MANIFEST.in
    C:\temp\load\setup.py

---
+++ build/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""Setup script for the pyparsing module distribution."""
+from distutils.core import setup
+
+from parsing import __version__
+
+setup(# Distribution meta-data
+    name = "pyparsing",
+    version = "1.0.3",
+    description = "Python parsing module",
+    author = "Paul McGuire",
+    author_email = "ptmcg@users.sourceforge.net",
+    url = "http://pyparsing.sourceforge.net/",
+    license = "MIT License",
+    py_modules = ["pyparsing"],
+    )
