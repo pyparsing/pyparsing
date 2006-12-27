89c76acd6682ab1b6f76846620bc1d9207c65e9b
Paul <Paul@9bf210a0-9d2d-494c-87cf-cfb32e7dff7b>
2006-12-27 17:49:48 +0000

    Added support for simplified exception syntax:   raise ParseFatalException, "reason" (provides defaults for pstr and loc values)

--- src/pyparsing.py
+++ src/pyparsing.py
@@ -112,10 +112,14 @@ class ParseBaseException(Exception):
     __slots__ = ( "loc","msg","pstr","parserElement" )
     # Performance tuning: we construct a *lot* of these, so keep this
     # constructor as small and fast as possible        
-    def __init__( self, pstr, loc, msg, elem=None ):
+    def __init__( self, pstr, loc=0, msg=None, elem=None ):
         self.loc = loc
-        self.msg = msg
-        self.pstr = pstr
+        if msg is None:
+            self.msg = pstr
+            self.pstr = ""
+        else:
+            self.msg = msg
+            self.pstr = pstr
         self.parserElement = elem
 
     def __getattr__( self, aname ):
