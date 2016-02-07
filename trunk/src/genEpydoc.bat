a4b65b0c2080265e53508e70def539a4a0be38cd
ptmcg <ptmcg@9bf210a0-9d2d-494c-87cf-cfb32e7dff7b>
2016-02-07 19:48:55 +0000

    Fixed to match current Python installed version

--- trunk/src/genEpydoc.bat
+++ trunk/src/genEpydoc.bat
@@ -1,2 +1,2 @@
 xcopy /y .\sourceforge\svn\trunk\src\pyparsing.py .
-python c:\python26\scripts\epydoc -v --name pyparsing -o htmldoc --inheritance listed --no-private pyparsing.py
+python c:\python27\scripts\epydoc -v --name pyparsing -o htmldoc --inheritance listed --no-private pyparsing.py
