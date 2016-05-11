d455e7ee6f60854664cef0c3f257d818b487092b
ptmcg <ptmcg@9bf210a0-9d2d-494c-87cf-cfb32e7dff7b>
2016-05-11 15:15:17 +0000

    traceback module variations between Py3.5.0 and Py3.5.1

--- trunk/src/pyparsing.py
+++ trunk/src/pyparsing.py
@@ -788,9 +788,12 @@ def _trim_arity(func, maxargs=2):
     foundArity = [False]
     
     # traceback return data structure changed in Py3.5 - normalize back to plain tuples
-    if tuple(sys.version_info) >= (3,5):
+    ver = tuple(sys.version_info)[:3]
+    if ver[:2] >= (3,5):
         def extract_stack():
-            frame_summary = traceback.extract_stack()[-3]
+            # special handling for Python 3.5.0 - extra deep call stack by 1
+            offset = -3 if ver == (3,5,0) else -2
+            frame_summary = traceback.extract_stack()[offset]
             return [(frame_summary.filename, frame_summary.lineno)]
         def extract_tb(tb):
             frames = traceback.extract_tb(tb)
