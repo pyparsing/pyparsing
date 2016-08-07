93d0d9a079655afebc3121e3bc59dde7b8c8d308
ptmcg <ptmcg@9bf210a0-9d2d-494c-87cf-cfb32e7dff7b>
2016-08-07 04:29:07 +0000

    Update indentedGrammarExample.py to use indentedBlock Remove confusing example tagCapture.py

--- trunk/src/examples/indentedGrammarExample.py
+++ trunk/src/examples/indentedGrammarExample.py
@@ -1,9 +1,11 @@
 # indentedGrammarExample.py
 #
-# Copyright (c) 2006, Paul McGuire
+# Copyright (c) 2006,2016  Paul McGuire
 #
 # A sample of a pyparsing grammar using indentation for 
 # grouping (like Python does).
+#
+# Updated to use indentedBlock helper method.
 #
 
 from pyparsing import *
@@ -29,41 +31,14 @@ def spam(x,y):
          pass
 """
 
+
 indentStack = [1]
-
-def checkPeerIndent(s,l,t):
-    curCol = col(l,s)
-    if curCol != indentStack[-1]:
-        if (not indentStack) or curCol > indentStack[-1]:
-            raise ParseFatalException(s,l,"illegal nesting")
-        raise ParseException(s,l,"not a peer entry")
-
-def checkSubIndent(s,l,t):
-    curCol = col(l,s)
-    if curCol > indentStack[-1]:
-        indentStack.append( curCol )
-    else:
-        raise ParseException(s,l,"not a subentry")
-
-def checkUnindent(s,l,t):
-    if l >= len(s): return
-    curCol = col(l,s)
-    if not(curCol < indentStack[-1] and curCol <= indentStack[-2]):
-        raise ParseException(s,l,"not an unindent")
-
-def doUnindent():
-    indentStack.pop()
-    
-INDENT = lineEnd.suppress() + empty + empty.copy().setParseAction(checkSubIndent)
-UNDENT = FollowedBy(empty).setParseAction(checkUnindent)
-UNDENT.setParseAction(doUnindent)
-
 stmt = Forward()
-suite = Group( OneOrMore( empty + stmt.setParseAction( checkPeerIndent ) )  )
+suite = indentedBlock(stmt, indentStack)
 
 identifier = Word(alphas, alphanums)
 funcDecl = ("def" + identifier + Group( "(" + Optional( delimitedList(identifier) ) + ")" ) + ":")
-funcDef = Group( funcDecl + INDENT + suite + UNDENT )
+funcDef = Group( funcDecl + suite )
 
 rvalue = Forward()
 funcCall = Group(identifier + "(" + Optional(delimitedList(rvalue)) + ")")
@@ -71,8 +46,9 @@ rvalue << (funcCall | identifier | Word(nums))
 assignment = Group(identifier + "=" + rvalue)
 stmt << ( funcDef | assignment | identifier )
 
-print(data)
-parseTree = suite.parseString(data)
+module_body = OneOrMore(stmt)
 
-import pprint
-pprint.pprint( parseTree.asList() )
+print(data)
+parseTree = module_body.parseString(data)
+parseTree.pprint()
+
