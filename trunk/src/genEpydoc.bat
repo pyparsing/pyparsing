f7546525c1e7be0c94f1227b75d7654c2d5cfca9
ptmcg <ptmcg@9bf210a0-9d2d-494c-87cf-cfb32e7dff7b>
2016-08-07 04:43:23 +0000

    Fix relative dir typo in xcopy pyparsing.py command

--- trunk/src/genEpydoc.bat
+++ trunk/src/genEpydoc.bat
@@ -1,2 +1,2 @@
-xcopy /y .\sourceforge\svn\trunk\src\pyparsing.py .
+xcopy /y ..\sourceforge\svn\trunk\src\pyparsing.py .
 c:\python27\python c:\python27\scripts\epydoc -v --name pyparsing -o htmldoc --inheritance listed --no-private pyparsing.py
