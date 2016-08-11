ddd6e09d7830b461a0437f709d1844f186614dec
ptmcg <ptmcg@9bf210a0-9d2d-494c-87cf-cfb32e7dff7b>
2016-08-11 18:41:57 +0000

    Tag for 2.1.7 release

---
+++ tags/pyparsing_2.1.7/src/examples/holaMundo.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+
+# escrito por Marco Alfonso, 2004 Noviembre
+
+# importamos el modulo 
+from pyparsing import * 
+saludo= Word(alphas) + ',' + Word(alphas) + '!' 
+ 
+# Aqui decimos que la gramatica "saludo" DEBE contener 
+# una palabra compuesta de caracteres alfanumericos 
+# (Word(alphas)) mas una ',' mas otra palabra alfanumerica, 
+# mas '!' y esos seian nuestros tokens 
+tokens = saludo.parseString("Hola, Mundo !") 
+ 
+# Ahora parseamos una cadena, "Hola, Mundo!", 
+# el metodo parseString, nos devuelve una lista con los tokens 
+# encontrados, en caso de no haber errores... 
+for i in range(len(tokens)):
+    print ("Token %d -> %s" % (i,tokens[i]))
+
+#imprimimos cada uno de los tokens Y listooo!!, he aquí a salida 
+# Token 0 -> Hola
+# Token 1 -> ,
+# Token 2-> Mundo 
+# Token 3 -> ! 
+ 
+# Por supuesto, se pueden "reutilizar" gramáticas, por ejemplo: 
+numimag = Word(nums) + 'i' 
+numreal = Word(nums) 
+numcomplex = numreal + '+' + numimag 
+print (numcomplex.parseString("3+5i"))
+
+# Cambiar a complejo numero durante parsear:
+numcomplex.setParseAction(lambda t: complex(''.join(t).replace('i','j')))
+print (numcomplex.parseString("3+5i"))
+
+# Excelente!!, bueno, los dejo, me voy a seguir tirando código...
+
---
+++ tags/pyparsing_2.1.7/src/genEpydoc.bat
@@ -0,0 +1,2 @@
+xcopy /y ..\sourceforge\svn\trunk\src\pyparsing.py .
+c:\python27\python c:\python27\scripts\epydoc -v --name pyparsing -o htmldoc --inheritance listed --no-private pyparsing.py
