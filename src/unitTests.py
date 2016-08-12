70bd155a711fcc18907ba1b0e2850fd3c8eb84d1
Paul McGuire <ptmcg@austin.rr.com>
2016-08-12 14:48:18 +0000

    Add limit=n args to extract_stack and extract_tb calls, to minimize scanning through full call stack - trying to address UnicodeDecodeError exceptions from loading in more code than we need.

--- src/unitTests.py
+++ src/unitTests.py
@@ -2529,6 +2529,58 @@ class TrimArityExceptionMaskingTest(ParseTestCase):
             exc_msg = str(e)
         assert exc_msg != invalid_message, "failed to catch TypeError thrown in _trim_arity"
 
+class TrimArityExceptionMaskingTest2(ParseTestCase):
+    def runTest(self):
+        
+        
+        # construct deep call tree
+        def A():
+            import traceback
+            
+            traceback.print_stack(limit=2)
+
+            from pyparsing import Word
+            
+            invalid_message = [
+                "<lambda>() takes exactly 1 argument (0 given)",
+                "<lambda>() missing 1 required positional argument: 't'"
+                ][PY_3]
+            try:
+                Word('a').setParseAction(lambda t: t[0]+1).parseString('aaa')
+            except Exception as e:
+                exc_msg = str(e)
+            assert exc_msg != invalid_message, "failed to catch TypeError thrown in _trim_arity"
+
+            
+        def B():
+            A()
+            
+        def C():
+            B()
+            
+        def D():
+            C()
+            
+        def E():
+            D()
+            
+        def F():
+            E()
+            
+        def G():
+            F()
+            
+        def H():
+            G()
+            
+        def J():
+            H()
+            
+        def K():
+            J()
+
+        K()
+
 class OneOrMoreStopTest(ParseTestCase):
     def runTest(self):
         from pyparsing import (Word, OneOrMore, alphas, Keyword, CaselessKeyword,
