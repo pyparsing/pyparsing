a976a1a7beb9eae44573480e0d41f8c6dce9c3a5
ptmcg <ptmcg@9bf210a0-9d2d-494c-87cf-cfb32e7dff7b>
2016-08-15 22:35:11 +0000

    Tag for 2.1.8 release

---
+++ tags/pyparsing_2.1.8/src/examples/greetingInGreek.py
@@ -0,0 +1,20 @@
+# vim:fileencoding=utf-8 
+#
+# greetingInGreek.py
+#
+# Demonstration of the parsing module, on the prototypical "Hello, World!" example
+#
+# Copyright 2004-2016, by Paul McGuire
+#
+from pyparsing import Word 
+
+# define grammar
+alphas = ''.join(chr(x) for x in range(0x386, 0x3ce)) 
+greet = Word(alphas) + ',' + Word(alphas) + '!' 
+
+# input string
+hello = "Καλημέρα, κόσμε!".decode('utf-8') 
+
+# parse input string
+print(greet.parseString( hello ))
+
