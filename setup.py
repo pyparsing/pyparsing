6a62954c02083b4a07b98f29cedc9b190732c760
Hugo <hugovk@users.noreply.github.com>
2018-08-09 17:12:05 +0300

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(# Distribution meta-data
     download_url = "https://pypi.org/project/pyparsing/",
     license = "MIT License",
     py_modules = modules,
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
