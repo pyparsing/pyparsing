bb9d1255548b46dc2ba7a85e26606b7dd4c926f3
Paul McGuire <ptmcg@austin.rr.com>
2019-03-30 02:59:39 -0500

    Update original "Hello, World!" parser to latest coding, plus runTests

--- examples/greeting.py
+++ examples/greeting.py
@@ -3,15 +3,23 @@
 # Demonstration of the pyparsing module, on the prototypical "Hello, World!"
 # example
 #
-# Copyright 2003, by Paul McGuire
+# Copyright 2003, 2019 by Paul McGuire
 #
-from pyparsing import Word, alphas
+import pyparsing as pp
 
 # define grammar
-greet = Word( alphas ) + "," + Word( alphas ) + "!"
+greet = pp.Word(pp.alphas) + "," + pp.Word(pp.alphas) + pp.oneOf("! ? .")
 
 # input string
 hello = "Hello, World!"
 
 # parse input string
 print(hello, "->", greet.parseString( hello ))
+
+# parse a bunch of input strings
+greet.runTests("""\
+    Hello, World!
+    Ahoy, Matey!
+    Howdy, Pardner!
+    Morning, Neighbor!
+    """)
