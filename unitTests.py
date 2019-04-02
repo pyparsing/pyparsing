639e64d5da626a12f01d077797587b0cc31c231b
Paul McGuire <ptmcg@austin.rr.com>
2019-04-01 23:13:09 -0500

    Remove disabling of tests for Py2 when using explain

--- unitTests.py
+++ unitTests.py
@@ -3980,10 +3980,6 @@ class ParseResultsWithNameOr(ParseTestCase):
 
 class EmptyDictDoesNotRaiseException(ParseTestCase):
     def runTest(self):
-        if not PY_3:
-            print('explain() not supported in Py2')
-            return
-
         import pyparsing as pp
 
         key = pp.Word(pp.alphas)
@@ -4005,10 +4001,6 @@ class EmptyDictDoesNotRaiseException(ParseTestCase):
 
 class ExplainExceptionTest(ParseTestCase):
     def runTest(self):
-        if not PY_3:
-            print('explain() not supported in Py2')
-            return
-
         import pyparsing as pp
 
         expr = pp.Word(pp.nums).setName("int") + pp.Word(pp.alphas).setName("word")
