39934574db9796f27397b9f76a112c2466bd1a69
Paul McGuire <ptmcg@users.noreply.github.com>
2020-04-28 23:10:36 -0500

    remove Regex docstring notes that reference no-longer-supported replacement of internal import of re with other RE modules such as regex

--- pyparsing/core.py
+++ pyparsing/core.py
@@ -2289,30 +2289,21 @@ class Regex(Token):
     these will be preserved as named :class:`ParseResults`.
 
     If instead of the Python stdlib re module you wish to use a different RE module
-    (such as the `regex` module), you can replace it by either building your
-    Regex object with a compiled RE that was compiled using regex, or by replacing
-    the imported `re` module in pyparsing with the `regex` module:
-
+    (such as the `regex` module), you can do so by building your Regex object with
+    a compiled RE that was compiled using regex.
 
     Example::
 
         realnum = Regex(r"[+-]?\d+\.\d*")
-        date = Regex(r'(?P<year>\d{4})-(?P<month>\d\d?)-(?P<day>\d\d?)')
         # ref: https://stackoverflow.com/questions/267399/how-do-you-match-only-valid-roman-numerals-with-a-regular-expression
         roman = Regex(r"M{0,4}(CM|CD|D?{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})")
 
+        # named fields in a regex will be returned as named results
+        date = Regex(r'(?P<year>\d{4})-(?P<month>\d\d?)-(?P<day>\d\d?)')
+
+        # the Regex class will accept re's compiled using the regex module
         import regex
         parser = pp.Regex(regex.compile(r'[0-9]'))
-
-        # or
-
-        import pyparsing
-        pyparsing.re = regex
-
-        # both of these will use the regex module to compile their internal re's
-        parser = pp.Regex(r'[0-9]')
-        parser = pp.Word(pp.nums)
-
     """
 
     def __init__(self, pattern, flags=0, asGroupList=False, asMatch=False):
