bd6962c239748584fc451141543e420383dccf24
Marcin Jaworski <marcin@jaworski.me>
2020-05-03 16:04:02 +0200

    Fix operator precedence

--- pyparsing/helpers.py
+++ pyparsing/helpers.py
@@ -55,7 +55,7 @@ def countedArray(expr, intExpr=None):
 
     def countFieldParseAction(s, l, t):
         n = t[0]
-        arrayExpr << And([expr] * n) if n else empty
+        arrayExpr << (And([expr] * n) if n else empty)
         # clear list contents, but keep any named results
         del t[:]
 
