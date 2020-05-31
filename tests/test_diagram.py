58c171bb5077f615dc36fc55f470a462e56da891
Michael Milton <ttmigueltt@gmail.com>
2020-06-01 03:34:41 +1000

    Railroad Diagrams (#218)
    * Basic framework
    
    * Initial effort
    
    * Clean up and document code
    
    * jinja newline
    
    * Pre-commit, and add extras to tox
    
    * We can't use the class type-annotations syntax in Python 3.5

---
+++ tests/test_diagram.py
@@ -0,0 +1,38 @@
+import unittest
+from examples.jsonParser import jsonObject
+from examples.simpleBool import boolExpr
+from pyparsing.diagram import to_railroad, railroad_to_html
+import tempfile
+import os
+
+
+class TestRailroadDiagrams(unittest.TestCase):
+    def railroad_debug(self) -> bool:
+        """
+        Returns True if we're in debug mode
+        """
+        return os.environ.get("RAILROAD_DEBUG", False)
+
+    def get_temp(self):
+        """
+        Returns an appropriate temporary file for writing a railroad diagram
+        """
+        return tempfile.NamedTemporaryFile(
+            delete=not self.railroad_debug(), mode="w", encoding="utf-8", suffix=".html"
+        )
+
+    def test_bool_expr(self):
+        with self.get_temp() as temp:
+            railroad = to_railroad(boolExpr)
+            temp.write(railroad_to_html(railroad))
+
+            if self.railroad_debug():
+                print(temp.name)
+
+    def test_json(self):
+        with self.get_temp() as temp:
+            railroad = to_railroad(jsonObject)
+            temp.write(railroad_to_html(railroad))
+
+            if self.railroad_debug():
+                print(temp.name)
