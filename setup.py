d27fd7627f3ed60b3b7a9e9f5e790d0e49107359
ptmcg <ptmcg@austin.rr.com>
2021-04-13 10:12:15 -0500

    Remove setuptools import fallback to distutils.core (distutils.core use not recommended)

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@
 
 """Setup script for the pyparsing module distribution."""
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 import io
 import sys
 from pyparsing import __version__ as pyparsing_version
