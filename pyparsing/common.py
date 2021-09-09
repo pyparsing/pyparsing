e2fb9f25431544b3c783e13e7fffc0e17bcf9fc8
ptmcg <ptmcg@austin.rr.com>
2021-09-08 22:51:46 -0500

    Add url expression to pyparsing_common (#249)

--- pyparsing/common.py
+++ pyparsing/common.py
@@ -358,6 +358,54 @@ class pyparsing_common:
     downcase_tokens = staticmethod(token_map(lambda t: t.lower()))
     """Parse action to convert tokens to lower case."""
 
+    url = Regex(
+        # https://mathiasbynens.be/demo/url-regex
+        # https://gist.github.com/dperini/729294
+        r"^" +
+        # protocol identifier (optional)
+        # short syntax // still required
+        r"(?:(?:(?P<scheme>https?|ftp):)?\/\/)" +
+        # user:pass BasicAuth (optional)
+        r"(?:(?P<auth>\S+(?::\S*)?)@)?" +
+        r"(?P<host>" +
+        # IP address exclusion
+        # private & local networks
+        r"(?!(?:10|127)(?:\.\d{1,3}){3})" +
+        r"(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})" +
+        r"(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})" +
+        # IP address dotted notation octets
+        # excludes loopback network 0.0.0.0
+        # excludes reserved space >= 224.0.0.0
+        # excludes network & broadcast addresses
+        # (first & last IP address of each class)
+        r"(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])" +
+        r"(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}" +
+        r"(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))" +
+        r"|" +
+        # host & domain names, may end with dot
+        # can be replaced by a shortest alternative
+        # (?![-_])(?:[-\w\u00a1-\uffff]{0,63}[^-_]\.)+
+        r"(?:" +
+        r"(?:" +
+        r"[a-z0-9\u00a1-\uffff]" +
+        r"[a-z0-9\u00a1-\uffff_-]{0,62}" +
+        r")?" +
+        r"[a-z0-9\u00a1-\uffff]\." +
+        r")+" +
+        # TLD identifier name, may end with dot
+        r"(?:[a-z\u00a1-\uffff]{2,}\.?)" +
+        r")" +
+        # port number (optional)
+        r"(?P<port>:\d{2,5})?" +
+        # resource path (optional)
+        r"(?P<path>\/[^?# ]*)?" +
+        # query string (optional)
+        r"(?P<query>\?[^#]*)?" +
+        # fragment (optional)
+        r"(?P<fragment>#\S*)?" +
+        r"$"
+    ).set_name("url")
+
     # pre-PEP8 compatibility names
     convertToInteger = convert_to_integer
     convertToFloat = convert_to_float
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -5500,6 +5500,86 @@ class Test02_WithoutPackrat(ppt.TestParseResultsAsserts, TestCase):
                 ),
             )
 
+    def testCommonUrl(self):
+        url_good_tests = """\
+            http://foo.com/blah_blah
+            http://foo.com/blah_blah/
+            http://foo.com/blah_blah_(wikipedia)
+            http://foo.com/blah_blah_(wikipedia)_(again)
+            http://www.example.com/wpstyle/?p=364
+            https://www.example.com/foo/?bar=baz&inga=42&quux
+            http://✪df.ws/123
+            http://userid:password@example.com:8080
+            http://userid:password@example.com:8080/
+            http://userid@example.com
+            http://userid@example.com/
+            http://userid@example.com:8080
+            http://userid@example.com:8080/
+            http://userid:password@example.com
+            http://userid:password@example.com/
+            http://142.42.1.1/
+            http://142.42.1.1:8080/
+            http://➡.ws/䨹
+            http://⌘.ws
+            http://⌘.ws/
+            http://foo.com/blah_(wikipedia)#cite-1
+            http://foo.com/blah_(wikipedia)_blah#cite-1
+            http://foo.com/unicode_(✪)_in_parens
+            http://foo.com/(something)?after=parens
+            http://☺.damowmow.com/
+            http://code.google.com/events/#&product=browser
+            http://j.mp
+            ftp://foo.bar/baz
+            http://foo.bar/?q=Test%20URL-encoded%20stuff
+            http://مثال.إختبار
+            """
+        success, report = ppc.url.runTests(url_good_tests)
+        self.assertTrue(success)
+
+        url_bad_tests = """\
+            http://
+            http://.
+            http://..
+            http://../
+            http://?
+            http://??
+            http://??/
+            http://#
+            http://##
+            http://##/
+            # skip: http://foo.bar?q=Spaces should be encoded
+            //
+            //a
+            ///a
+            ///
+            http:///a
+            foo.com
+            rdar://1234
+            h://test
+            http:// shouldfail.com
+
+            :// should fail
+            http://foo.bar/foo(bar)baz quux
+            ftps://foo.bar/
+            http://-error-.invalid/
+            # skip: http://a.b--c.de/
+            http://-a.b.co
+            http://a.b-.co
+            http://0.0.0.0
+            http://10.1.1.0
+            http://10.1.1.255
+            http://224.1.1.1
+            http://1.1.1.1.1
+            http://123.123.123
+            http://3628126748
+            http://.www.foo.bar/
+            # skip: http://www.foo.bar./
+            http://.www.foo.bar./
+            http://10.1.1.1
+            """
+        success, report = ppc.url.runTests(url_bad_tests, failure_tests=True)
+        self.assertTrue(success)
+
     def testNumericExpressions(self):
 
         # disable parse actions that do type conversion so we don't accidentally trigger
