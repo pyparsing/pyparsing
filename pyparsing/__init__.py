b0657d9f98336eb232176d9117b443effa58ef95
ptmcg <ptmcg@austin.rr.com>
2021-10-02 00:29:43 -0500

    Bump version timestamp

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -105,7 +105,7 @@ __version__ = "{}.{}.{}".format(*__version_info__[:3]) + (
     ),
     "",
 )[__version_info__.release_level == "final"]
-__version_time__ = "28 September 2021 07:21 UTC"
+__version_time__ = "2 October 2021 05:29 UTC"
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg@users.sourceforge.net>"
 
