ac7d5a9f3c7a4ff5f617f7488944aaad63c4e47f
ptmcg <ptmcg@austin.rr.com>
2021-10-25 10:11:09 -0500

    Update version for next release work

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -96,7 +96,7 @@ classes inherit from. Use the docstrings for examples of how to:
 from collections import namedtuple
 
 version_info = namedtuple("version_info", "major minor micro release_level serial")
-__version_info__ = version_info(3, 0, 1, "final", 0)
+__version_info__ = version_info(3, 0, 2, "final", 0)
 __version__ = "{}.{}.{}".format(*__version_info__[:3]) + (
     "{}{}{}".format(
         "r" if __version_info__.release_level[0] == "c" else "",
@@ -105,7 +105,7 @@ __version__ = "{}.{}.{}".format(*__version_info__[:3]) + (
     ),
     "",
 )[__version_info__.release_level == "final"]
-__version_time__ = "24 October 2021 17:43 UTC"
+__version_time__ = "25 October 2021 15:10 UTC"
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg.gm+pyparsing@gmail.com>"
 
