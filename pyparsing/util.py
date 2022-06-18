3394e81df2c8cb4b1449834c9367d5f7cf6a819f
ptmcg <ptmcg@austin.rr.com>
2022-06-18 00:09:44 -0500

    Replace OrderedDict in FIFOCache with dict+key ringbuffer

--- pyparsing/util.py
+++ pyparsing/util.py
@@ -100,19 +100,24 @@ class _UnboundedCache:
 class _FifoCache:
     def __init__(self, size):
         self.not_in_cache = not_in_cache = object()
-        cache = collections.OrderedDict()
+        cache = {}
+        keyring = [object()] * size
         cache_get = cache.get
+        cache_pop = cache.pop
+        keyiter = itertools.cycle(range(size))
 
         def get(_, key):
             return cache_get(key, not_in_cache)
 
         def set_(_, key, value):
             cache[key] = value
-            while len(cache) > size:
-                cache.popitem(last=False)
+            i = next(keyiter)
+            cache_pop(keyring[i], None)
+            keyring[i] = key
 
         def clear(_):
             cache.clear()
+            keyring[:] = [object()] * size
 
         self.size = size
         self.get = types.MethodType(get, self)
