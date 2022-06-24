ba937e19dcd7a4b20ecf4b079f8154e5cf5cc303
ptmcg <ptmcg@austin.rr.com>
2022-06-24 11:29:36 -0500

    Update latest version timestamp

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -121,7 +121,7 @@ class version_info(NamedTuple):
 
 
 __version_info__ = version_info(3, 0, 10, "final", 0)
-__version_time__ = "16 Jun 2022 23:12 UTC"
+__version_time__ = "24 Jun 2022 16:29 UTC"
 __version__ = __version_info__.__version__
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg.gm+pyparsing@gmail.com>"
