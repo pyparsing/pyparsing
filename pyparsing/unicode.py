6aef782b6d347db134bafa66004c60b42e42e427
ptmcg <ptmcg@austin.rr.com>
2022-11-06 16:47:20 -0600

    Add Unicode MIDDLE DOT to Latin1.identbodychars

--- pyparsing/unicode.py
+++ pyparsing/unicode.py
@@ -100,13 +100,13 @@ class unicode_set:
     def identbodychars(cls):
         """
         all characters in this range that are valid identifier body characters,
-        plus the digits 0-9
+        plus the digits 0-9, and · (Unicode MIDDLE DOT)
         """
         return "".join(
             sorted(
                 set(
                     cls.identchars
-                    + "0123456789"
+                    + "0123456789·"
                     + "".join(
                         [c for c in cls._chars_for_ranges if ("_" + c).isidentifier()]
                     )
