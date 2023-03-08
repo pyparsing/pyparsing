eb59a9f86f08158cdf995ff452dd1957dc060e36
ptmcg <ptmcg@austin.rr.com>
2023-03-07 19:10:52 -0600

    Staging for 3.1.0a1 release

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -121,7 +121,7 @@ class version_info(NamedTuple):
 
 
 __version_info__ = version_info(3, 1, 0, "alpha", 1)
-__version_time__ = "07 Mar 2023 06:50 UTC"
+__version_time__ = "08 Mar 2023 01:10 UTC"
 __version__ = __version_info__.__version__
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg.gm+pyparsing@gmail.com>"
