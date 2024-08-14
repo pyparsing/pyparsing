d7c163c1d9079b65bf5ecbc2d118932b9a7c497e
ptmcg <ptmcg@austin.rr.com>
2024-08-13 20:51:38 -0500

    Some minor code changes in chemical_formulas.py

--- examples/chemical_formulas.py
+++ examples/chemical_formulas.py
@@ -23,7 +23,7 @@ element = pp.Word(pp.alphas.upper(), pp.alphas.lower(), max=2).set_name("element
 #                 "E[rsu]|F[emr]?|G[ade]|H[efgos]?|I[nr]?|Kr?|L[airu]|"
 #                 "M[dgnot]|N[abdeiop]?|Os?|P[abdmortu]?|R[abefghnu]|"
 #                 "S[bcegimnr]?|T[abcehilm]|U(u[bhopqst])?|V|W|Xe|Yb?|Z[nr]")
-element_ref = pp.Group(element + pp.Opt(pp.Word(digits), default="1"))
+element_ref = pp.Group(element + pp.Optional(pp.Word(digits), default="1"))
 formula = element_ref[...]
 
 
@@ -33,9 +33,9 @@ def sum_atomic_weights(element_list):
 
 formula.run_tests(
     """\
+    NaCl
     H2O
     C6H5OH
-    NaCl
     """,
     full_dump=False,
     post_parse=lambda _, tokens: f"Molecular weight: {sum_atomic_weights(tokens)}",
@@ -45,7 +45,7 @@ print()
 
 # Version 2 - access parsed items by results name
 element_ref = pp.Group(
-    element("symbol") + pp.Opt(pp.Word(digits), default="1")("qty")
+    element("symbol") + pp.Optional(pp.Word(digits), default="1")("qty")
 )
 formula = element_ref[...]
 
@@ -56,9 +56,9 @@ def sum_atomic_weights_by_results_name(element_list):
 
 formula.run_tests(
     """\
+    NaCl
     H2O
     C6H5OH
-    NaCl
     """,
     full_dump=False,
     post_parse=lambda _, tokens:
@@ -69,7 +69,7 @@ print()
 # Version 3 - convert integers during parsing process
 integer = pp.Word(digits).set_name("integer")
 integer.add_parse_action(lambda t: int(t[0]))
-element_ref = pp.Group(element("symbol") + pp.Opt(integer, default=1)("qty"))
+element_ref = pp.Group(element("symbol") + pp.Optional(integer, default=1)("qty"))
 formula = element_ref[...].set_name("chemical_formula")
 
 
@@ -79,9 +79,9 @@ def sum_atomic_weights_by_results_name_with_converted_ints(element_list):
 
 formula.run_tests(
     """\
+    NaCl
     H2O
     C6H5OH
-    NaCl
     """,
     full_dump=False,
     post_parse=lambda _, tokens:
@@ -91,7 +91,7 @@ print()
 
 # Version 4 - parse and convert integers as subscript digits
 subscript_digits = "₀₁₂₃₄₅₆₇₈₉"
-subscript_int_map = {e[1]: e[0] for e in enumerate(subscript_digits)}
+subscript_int_map = {digit: value for value, digit in enumerate(subscript_digits)}
 
 
 def cvt_subscript_int(s):
@@ -104,13 +104,20 @@ def cvt_subscript_int(s):
 subscript_int = pp.Word(subscript_digits).set_name("subscript")
 subscript_int.add_parse_action(cvt_subscript_int)
 
-element_ref = pp.Group(element("symbol") + pp.Opt(subscript_int, default=1)("qty"))
+element_ref = pp.Group(element("symbol") + pp.Optional(subscript_int, default=1)("qty"))
 formula = element_ref[1, ...].set_name("chemical_formula")
 formula.run_tests(
     """\
-    H₂O
-    C₆H₅OH
+    # sodium chloride
     NaCl
+    # hydrogen hydroxide
+    H₂O
+    # phenol
+    C₆H₅OH
+    # ethanol
+    C₂H₅OH
+    # decanol
+    C₁₀H₂₁OH
     """,
     full_dump=False,
     post_parse=lambda _, tokens:
