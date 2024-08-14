cf41d902772c169ca7740e99211c9f7d0163a753
ptmcg <ptmcg@austin.rr.com>
2024-08-13 20:56:02 -0500

    Prep for 3.1.3 release

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -121,7 +121,7 @@ class version_info(NamedTuple):
 
 
 __version_info__ = version_info(3, 1, 3, "final", 1)
-__version_time__ = "09 Mar 2024 15:40 UTC"
+__version_time__ = "14 Aug 2024 01:52 UTC"
 __version__ = __version_info__.__version__
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg.gm+pyparsing@gmail.com>"
