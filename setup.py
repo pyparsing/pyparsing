067999a52db00f0730f86cd65c73cab1822c01ac
Daniel Heilper <daniel.heilper@honeydew.ai>
2024-10-14 17:52:49 +0300

    make the build parallel. add profile flag for compiler (compiler guided optimization

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 from Cython.Build import cythonize
 import os
+from Cython.Distutils import build_ext as cython_build_ext
+import multiprocessing
 
 # This will find all .py files in the pyparsing package
 py_files = [os.path.join("pyparsing", file) for file in os.listdir("pyparsing") if file.endswith(".py")]
@@ -13,14 +15,23 @@ base_compiler_directives = {
     "infer_types": True,
     "embedsignature": True,
     "c_api_binop_methods": True,
+    "profile": True,
 }
 
-# Cythonize those files
-extensions = cythonize(py_files, compiler_directives=base_compiler_directives)
+# Get the number of physical cores
+num_cores = multiprocessing.cpu_count()
+extensions = cythonize(py_files, compiler_directives=base_compiler_directives, nthreads=8)
+
+# Custom build_ext command to include -j option
+class build_ext(cython_build_ext):
+    def initialize_options(self):
+        super().initialize_options()
+        self.parallel = num_cores
+
 
 setup(
     name="pyparsing",
     version="3.1.4",
     ext_modules=extensions,
-    
+    cmdclass={'build_ext': build_ext},    
 )
