8192211f05ce3a0966d531f7af4cfea81939b414
Baruch Oxman <baruch@honeydew.ai>
2024-10-30 00:25:46 +0200

    Add flto flag

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ import os
 from Cython.Distutils import build_ext as cython_build_ext
 import multiprocessing
 
-os.environ['CFLAGS'] = '-s'
+os.environ["CFLAGS"] = "-s -flto"
+os.environ["LDFLAGS"] = "-flto"
 # Define the base directory
 base_dir = "pyparsing"
 
@@ -27,13 +28,15 @@ base_compiler_directives = {
 }
 
 # Extra compile and link arguments
-extra_compile_args = [] # ["-O0", "-g"]  # Disable optimizations, include debug symbols
+extra_compile_args = []  # ["-O0", "-g"]  # Disable optimizations, include debug symbols
 extra_link_args = []
 
 # Create Extension objects
 extensions = []
 for py_file in py_files:
-    module_name = py_file.replace(os.path.sep, ".")[:-3]  # Convert file path to module name
+    module_name = py_file.replace(os.path.sep, ".")[
+        :-3
+    ]  # Convert file path to module name
     extensions.append(
         Extension(
             module_name,
@@ -42,11 +45,14 @@ for py_file in py_files:
             extra_link_args=extra_link_args,
         )
     )
-    
-extensions = cythonize(extensions, compiler_directives=base_compiler_directives, nthreads=8)
-    
+
+extensions = cythonize(
+    extensions, compiler_directives=base_compiler_directives, nthreads=8
+)
+
 # Get the number of physical cores
 num_cores = multiprocessing.cpu_count()
+
 
 # Custom build_ext command to include -j option
 class build_ext(cython_build_ext):
@@ -54,9 +60,10 @@ class build_ext(cython_build_ext):
         super().initialize_options()
         self.parallel = num_cores
 
+
 setup(
     name="pyparsing",
     version="3.2.0",
     ext_modules=extensions,
-    cmdclass={'build_ext': build_ext},    
+    cmdclass={"build_ext": build_ext},
 )
