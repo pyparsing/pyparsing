5db8be432b2ccef2db9f603d5d7745be32403710
ptmcg <ptmcg@austin.rr.com>
2025-01-23 12:06:24 -0600

    Updated ebnf.py to current pyparsing styles, optimized regex for quoted strings

--- examples/ebnf.py
+++ examples/ebnf.py
@@ -1,14 +1,16 @@
 # This module tries to implement ISO 14977 standard with pyparsing.
 # pyparsing version 1.1 or greater is required.
+from typing import Any
 
 # ISO 14977 standardize The Extended Backus-Naur Form(EBNF) syntax.
 # You can read a final draft version here:
 # https://www.cl.cam.ac.uk/~mgk25/iso-ebnf.html
 #
 # Submitted 2004 by Seo Sanghyeon
+# Updated to current pyparsing styles 2025 by Paul McGuire
 #
 
-from pyparsing import *
+import pyparsing as pp
 
 
 all_names = """
@@ -27,147 +29,160 @@ syntax_rule
 syntax
 """.split()
 
+LBRACK, RBRACK, LBRACE, RBRACE, LPAR, RPAR, DASH, STAR, EQ, SEMI = pp.Suppress.using_each(
+    "[]{}()-*=;"
+)
 
-integer = Word(nums)
-meta_identifier = Word(alphas, alphanums + "_")
-terminal_string = Suppress("'") + CharsNotIn("'") + Suppress("'") ^ Suppress(
-    '"'
-) + CharsNotIn('"') + Suppress('"')
-definitions_list = Forward()
-optional_sequence = Suppress("[") + definitions_list + Suppress("]")
-repeated_sequence = Suppress("{") + definitions_list + Suppress("}")
-grouped_sequence = Suppress("(") + definitions_list + Suppress(")")
+integer = pp.common.integer
+meta_identifier = pp.common.identifier
+terminal_string = pp.Regex(
+    r'"[^"]*"'
+    r"|"
+    r"'[^']*'"
+).add_parse_action(pp.remove_quotes)
+
+definitions_list = pp.Forward()
+optional_sequence = LBRACK + definitions_list + RBRACK
+repeated_sequence = LBRACE + definitions_list + RBRACE
+grouped_sequence = LPAR + definitions_list + RPAR
 syntactic_primary = (
     optional_sequence
-    ^ repeated_sequence
-    ^ grouped_sequence
-    ^ meta_identifier
-    ^ terminal_string
+    | repeated_sequence
+    | grouped_sequence
+    | meta_identifier
+    | terminal_string
 )
-syntactic_factor = Optional(integer + Suppress("*")) + syntactic_primary
-syntactic_term = syntactic_factor + Optional(Suppress("-") + syntactic_factor)
-single_definition = delimitedList(syntactic_term, ",")
-definitions_list << delimitedList(single_definition, "|")
-syntax_rule = meta_identifier + Suppress("=") + definitions_list + Suppress(";")
+syntactic_factor = pp.Optional(integer + STAR) + syntactic_primary
+syntactic_term = syntactic_factor + pp.Optional(DASH + syntactic_factor)
+single_definition = pp.DelimitedList(syntactic_term, ",")
+definitions_list <<= pp.DelimitedList(single_definition, "|")
+syntax_rule = meta_identifier + EQ + definitions_list + SEMI
 
 ebnfComment = (
-    ("(*" + ZeroOrMore(CharsNotIn("*") | ("*" + ~Literal(")"))) + "*)")
+    ("(*" + (pp.CharsNotIn("*") | ("*" + ~pp.Literal(")")))[...] + "*)")
     .streamline()
     .setName("ebnfComment")
 )
 
-syntax = OneOrMore(syntax_rule)
+syntax = syntax_rule[1, ...]
 syntax.ignore(ebnfComment)
 
 
-def do_integer(str, loc, toks):
+def do_integer(toks):
     return int(toks[0])
 
 
-def do_meta_identifier(str, loc, toks):
+def do_meta_identifier(toks):
     if toks[0] in symbol_table:
         return symbol_table[toks[0]]
     else:
         forward_count.value += 1
-        symbol_table[toks[0]] = Forward()
+        symbol_table[toks[0]] = pp.Forward()
         return symbol_table[toks[0]]
 
 
-def do_terminal_string(str, loc, toks):
-    return Literal(toks[0])
+def do_terminal_string(toks):
+    return pp.Literal(toks[0])
 
 
-def do_optional_sequence(str, loc, toks):
-    return Optional(toks[0])
+def do_optional_sequence(toks):
+    return pp.Optional(toks[0])
 
 
-def do_repeated_sequence(str, loc, toks):
-    return ZeroOrMore(toks[0])
+def do_repeated_sequence(toks):
+    return pp.ZeroOrMore(toks[0])
 
 
-def do_grouped_sequence(str, loc, toks):
-    return Group(toks[0])
+def do_grouped_sequence(toks):
+    return pp.Group(toks[0])
 
 
-def do_syntactic_primary(str, loc, toks):
+def do_syntactic_primary(toks):
     return toks[0]
 
 
-def do_syntactic_factor(str, loc, toks):
-    if len(toks) == 2:
+def do_syntactic_factor(toks):
+    if len(toks) == 2 and toks[0] > 1:
         # integer * syntactic_primary
-        return And([toks[1]] * toks[0])
+        return pp.And([toks[1]] * toks[0])
     else:
         # syntactic_primary
         return [toks[0]]
 
 
-def do_syntactic_term(str, loc, toks):
+def do_syntactic_term(toks):
     if len(toks) == 2:
         # syntactic_factor - syntactic_factor
-        return NotAny(toks[1]) + toks[0]
+        return pp.NotAny(toks[1]) + toks[0]
     else:
         # syntactic_factor
         return [toks[0]]
 
 
-def do_single_definition(str, loc, toks):
+def do_single_definition(toks):
     toks = toks.asList()
     if len(toks) > 1:
         # syntactic_term , syntactic_term , ...
-        return And(toks)
+        return pp.And(toks)
     else:
         # syntactic_term
         return [toks[0]]
 
 
-def do_definitions_list(str, loc, toks):
+def do_definitions_list(toks):
     toks = toks.asList()
     if len(toks) > 1:
         # single_definition | single_definition | ...
-        return Or(toks)
+        return pp.Or(toks)
     else:
         # single_definition
         return [toks[0]]
 
 
-def do_syntax_rule(str, loc, toks):
+def do_syntax_rule(toks):
     # meta_identifier = definitions_list ;
     assert toks[0].expr is None, "Duplicate definition"
     forward_count.value -= 1
-    toks[0] << toks[1]
+    toks[0] <<= toks[1]
     return [toks[0]]
 
 
-def do_syntax(str, loc, toks):
+def do_syntax():
     # syntax_rule syntax_rule ...
     return symbol_table
 
 
-symbol_table = {}
-
+symbol_table: dict[str, pp.Forward] = {}
 
 class forward_count:
     pass
-
-
 forward_count.value = 0
+
 for name in all_names:
     expr = vars()[name]
     action = vars()["do_" + name]
-    expr.setName(name)
-    expr.setParseAction(action)
-    # ~ expr.setDebug()
+    expr.set_name(name)
+    expr.add_parse_action(action)
+    # expr.setDebug()
 
 
-def parse(ebnf, given_table={}):
+def parse(ebnf, given_table=None):
+    given_table = given_table or {}
     symbol_table.clear()
     symbol_table.update(given_table)
     forward_count.value = 0
-    table = syntax.parseString(ebnf)[0]
-    assert forward_count.value == 0, "Missing definition"
+    table = syntax.parse_string(ebnf, parse_all=True)[0]
+    # assert forward_count.value == 0, "Missing definition"
     for name in table:
         expr = table[name]
-        expr.setName(name)
-        # ~ expr.setDebug()
+        expr.set_name(name)
+        # expr.set_debug()
     return table
+
+
+if __name__ == '__main__':
+    try:
+        syntax.create_diagram("ebnf_diagram.html")
+    except Exception as e:
+        print("Failed to create diagram for EBNF syntax parser"
+              f" - {type(e).__name__}: {e}")
--- examples/ebnftest.py
+++ examples/ebnftest.py
@@ -6,70 +6,53 @@
 # Submitted 2004 by Seo Sanghyeon
 #
 print("Importing pyparsing...")
-from pyparsing import *
+import pyparsing as pp
 
 print("Constructing EBNF parser with pyparsing...")
 import ebnf
 
 
 grammar = """
-syntax = (syntax_rule), {(syntax_rule)};
-syntax_rule = meta_identifier, '=', definitions_list, ';';
-definitions_list = single_definition, {'|', single_definition};
-single_definition = syntactic_term, {',', syntactic_term};
-syntactic_term = syntactic_factor,['-', syntactic_factor];
-syntactic_factor = [integer, '*'], syntactic_primary;
-syntactic_primary = optional_sequence | repeated_sequence |
-  grouped_sequence | meta_identifier | terminal_string;
-optional_sequence = '[', definitions_list, ']';
-repeated_sequence = '{', definitions_list, '}';
-grouped_sequence = '(', definitions_list, ')';
-(*
-terminal_string = "'", character - "'", {character - "'"}, "'" |
-  '"', character - '"', {character - '"'}, '"';
- meta_identifier = letter, {letter | digit};
-integer = digit, {digit};
-*)
+    (*
+        ISO 14977 standardize The Extended Backus-Naur Form(EBNF) syntax.
+        You can read a final draft version here:
+        https://www.cl.cam.ac.uk/~mgk25/iso-ebnf.html
+    *)
+    syntax = (syntax_rule), {(syntax_rule)};
+    syntax_rule = meta_identifier, '=', definitions_list, ';';
+    definitions_list = single_definition, {'|', single_definition};
+    single_definition = syntactic_term, {',', syntactic_term};
+    syntactic_term = syntactic_factor,['-', syntactic_factor];
+    syntactic_factor = [integer, '*'], syntactic_primary;
+    syntactic_primary = optional_sequence | repeated_sequence |
+      grouped_sequence | meta_identifier | terminal_string;
+    optional_sequence = '[', definitions_list, ']';
+    repeated_sequence = '{', definitions_list, '}';
+    grouped_sequence = '(', definitions_list, ')';
+    (*
+    terminal_string = "'", character - "'", {character - "'"}, "'" |
+      '"', character - '"', {character - '"'}, '"';
+     meta_identifier = letter, {letter | digit};
+    integer = digit, {digit};
+    *)
 """
 
-table = {}
-# ~ table['character'] = Word(printables, exact=1)
-# ~ table['letter'] = Word(alphas + '_', exact=1)
-# ~ table['digit'] = Word(nums, exact=1)
-table["terminal_string"] = sglQuotedString
-table["meta_identifier"] = Word(alphas + "_", alphas + "_" + nums)
-table["integer"] = Word(nums)
+table: dict[str, pp.ParserElement] = {}
+# table['character'] = Char(printables)
+# table['letter'] = Char(alphas + '_')
+# table['digit'] = Char(nums)
+table["terminal_string"] = pp.sgl_quoted_string | pp.dbl_quoted_string
+table["meta_identifier"] = pp.Word(pp.alphas + "_", pp.alphas + "_" + pp.nums)
+table["integer"] = pp.common.integer
 
 print("Parsing EBNF grammar with EBNF parser...")
 parsers = ebnf.parse(grammar, table)
 ebnf_parser = parsers["syntax"]
 
-commentcharcount = 0
-commentlocs = set()
-
-
-def tallyCommentChars(s, l, t):
-    global commentcharcount, commentlocs
-    # only count this comment if we haven't seen it before
-    if l not in commentlocs:
-        charCount = len(t[0]) - len(list(filter(str.isspace, t[0])))
-        commentcharcount += charCount
-        commentlocs.add(l)
-    return l, t
-
-
-# ordinarily, these lines wouldn't be necessary, but we are doing extra stuff with the comment expression
-ebnf.ebnfComment.setParseAction(tallyCommentChars)
 ebnf_parser.ignore(ebnf.ebnfComment)
 
+ebnf_parser.create_diagram("ebnftest_diagram.html")
+
 print("Parsing EBNF grammar with generated EBNF parser...\n")
-parsed_chars = ebnf_parser.parseString(grammar)
-parsed_char_len = len(parsed_chars)
-
-print("],\n".join(str(parsed_chars.asList()).split("],")))
-
-# ~ grammar_length = len(grammar) - len(filter(str.isspace, grammar))-commentcharcount
-
-# ~ assert parsed_char_len == grammar_length
-
-print("Ok!")
+parsed_chars = ebnf_parser.parse_string(grammar, parse_all=True)
+print("\n".join(str(pc) for pc in parsed_chars.as_list()))
