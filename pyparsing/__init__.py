cfdf9f5207fed3f4cd8a0638cf658d5b198f37f3
ptmcg <ptmcg@austin.rr.com>
2025-11-25 19:52:39 -0600

    Update version timestamp, change version to 3.3.0b1

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -156,8 +156,8 @@ class version_info(NamedTuple):
         return f"{__name__}.{type(self).__name__}({', '.join('{}={!r}'.format(*nv) for nv in zip(self._fields, self))})"
 
 
-__version_info__ = version_info(3, 3, 0, "alpha", 2)
-__version_time__ = "24 Nov 2025 23:19 UTC"
+__version_info__ = version_info(3, 3, 0, "beta", 1)
+__version_time__ = "26 Nov 2025 01:52 UTC"
 __version__ = __version_info__.__version__
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg.gm+pyparsing@gmail.com>"
