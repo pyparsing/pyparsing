6413afca276e2cb1b3a57e7f3bca55542ea366f5
ptmcg <ptmcg@austin.rr.com>
2025-12-21 09:56:00 -0600

    Prep for 3.3.0 release

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -1,26 +1,5 @@
-# module pyparsing.py
-#
-# Copyright (c) 2003-2022  Paul T. McGuire
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
+# see LICENSE file for terms and conditions for using this software.
+
 # fmt: off
 __doc__ = """
 pyparsing - Classes and methods to define and execute parsing grammars
@@ -157,7 +136,7 @@ class version_info(NamedTuple):
 
 
 __version_info__ = version_info(3, 3, 0, "final", 1)
-__version_time__ = "26 Nov 2025 08:53 UTC"
+__version_time__ = "21 Dec 2025 15:53 UTC"
 __version__ = __version_info__.__version__
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg.gm+pyparsing@gmail.com>"
