ecb1b14dbf1ad1b699c081ed63b9746a244ee5fa
ptmcg <ptmcg@austin.rr.com>
2026-06-30 03:22:18 -0500

    Add CHANGES blurb for ParseResults.__add__ and __radd__ fixes

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -136,7 +136,7 @@ class version_info(NamedTuple):
 
 
 __version_info__ = version_info(3, 3, 3, "final", 1)
-__version_time__ = "10 Apr 2026 03:49 UTC"
+__version_time__ = "30 Jun 2026 08:20 UTC"
 __version__ = __version_info__.__version__
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg.gm+pyparsing@gmail.com>"
