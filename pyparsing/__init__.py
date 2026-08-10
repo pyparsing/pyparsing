90c30252e6f52952eb4fdf0a7a4eb0980963415c
ptmcg <ptmcg@austin.rr.com>
2026-08-09 22:30:39 -0500

    Blackening

--- pyparsing/__init__.py
+++ pyparsing/__init__.py
@@ -136,7 +136,7 @@ class version_info(NamedTuple):
 
 
 __version_info__ = version_info(3, 3, 3, "final", 1)
-__version_time__ = "09 Aug 2026 21:49 UTC"
+__version_time__ = "10 Aug 2026 03:30 UTC"
 __version__ = __version_info__.__version__
 __versionTime__ = __version_time__
 __author__ = "Paul McGuire <ptmcg.gm+pyparsing@gmail.com>"
